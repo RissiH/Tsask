450bde9300f0ad4323a8c0097178ad97dfacff62
Rissi <nyxymproxy76@gmail.com>
2021-03-29 12:08:41 +0100

    Add project files.

---
+++ Tsask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tsask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }       
+    }
+}
+   
